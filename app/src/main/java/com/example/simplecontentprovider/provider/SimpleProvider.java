318197e5eab92ad1c03c173a843532518bd803b5
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-17 11:30:12 -0400

    Reorder SimpleProvider methods

--- app/src/main/java/com/example/simplecontentprovider/provider/SimpleProvider.java
+++ app/src/main/java/com/example/simplecontentprovider/provider/SimpleProvider.java
@@ -37,9 +37,9 @@ public class SimpleProvider extends ContentProvider {
     }
 
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public boolean onCreate() {
+        mDatabaseHelper = new DatabaseHelper(getContext());
+        return true;
     }
 
     @Override
@@ -54,6 +54,13 @@ public class SimpleProvider extends ContentProvider {
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
@@ -89,22 +96,16 @@ public class SimpleProvider extends ContentProvider {
     }
 
     @Override
-    public boolean onCreate() {
-        mDatabaseHelper = new DatabaseHelper(getContext());
-        return true;
-    }
-
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
         // TODO: Implement this to handle requests to update one or more rows.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
 }
